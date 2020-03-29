f8c7cee2a5bc81427c90a7aad02ed6b4c4465a9b
badung7576 <Dangbadung7576@gmail.com>
2020-03-29 21:47:43 +0700

    change code

--- compare.py
+++ compare.py
@@ -20,13 +20,6 @@ def get_dist_2vector(vector1, vector2):  # Hàm này tính khoảng cách giữa
     return dist
 
 
-# So sánh 2 vector theo cách của bạn anh
-def compare_2vector(vector1, vector2):
-    vector = vector1 / vector2
-    simi = np.average(vector) * 100
-    return simi
-
-
 # Hiện tại ảnh em đang giải nén ra và để trong thư mục images. Bên anh để ở thư mục nào thì sửa lại  IMAGES_PATH
 IMAGES_PATH = './images/=Signatures/=GFD/'
 
@@ -42,32 +35,10 @@ for name in filenames:
 # So sánh ảnh 1 với các ảnh còn lại và vẽ đồ thị.
 percents = []
 for x in filenames:
-    percents.append(compare_2vector(vectors['s01n001.gfd'], vectors[x]))
+    percents.append(get_dist_2vector(vectors['s05n001.gfd'], vectors[x]))
 
-plt.plot([_ for _ in range(len(percents))], percents, 'bx')
+plt.plot([_ for _ in range(len(percents))], percents, 'r1')
 plt.xlabel('Images')
-plt.ylabel('Percents')
+plt.ylabel('Distance')
 plt.show()
 
-# # dist là mảng 2 chiều : [distance, 0/1] : 0 là giống, 1 là khác.
-# dists = []
-# for x in filenames:
-#     for y in filenames:
-#         if x[0:3] == y[0:3]:
-#             if x != y:
-#                 dists.append([get_dist_2vector(vectors[x], vectors[y]), 0])
-#         else:
-#             dists.append([get_dist_2vector(vectors[x], vectors[y]), 1])
-#
-# dists = np.array(dists)
-# y = dists[:, -1]
-#
-# # simi là mảng các phần tử giống nhau, diff là mảng các phần tử khác nhau.
-# simi = dists[y == 0]
-# diff = dists[y == 1]
-#
-# # ở đây e chỉ vẽ khoảng 600 mẫu đầu tiên.
-# RANGE = 600
-# plt.plot(simi[:RANGE, 0], 'bx')
-# plt.plot(diff[:RANGE, 0], 'rx')
-# plt.show()
