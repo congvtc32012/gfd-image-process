742cd278b18b17b3f64fa433e22d31e4daf0504d
badung7576 <Dangbadung7576@gmail.com>
2020-03-29 22:25:43 +0700

    get similarity of 2 image

--- compare.py
+++ compare.py
@@ -1,5 +1,4 @@
 import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,7 +7,7 @@ def get_vector(path):
     try:
         img = open(path, 'r').readlines()
         vector = np.array([float(x.strip()) for x in img])
-    except IOError:  # Trường hợp đọc file lỗi thì ko xử lý, thông báo lỗi.
+    except IOError:     # Trường hợp đọc file lỗi thì ko xử lý, thông báo lỗi.
         vector = np.array([])
         print('Read file error!')
     return vector
@@ -16,29 +15,25 @@ def get_vector(path):
 
 def get_dist_2vector(vector1, vector2):  # Hàm này tính khoảng cách giữa 2 vector sử dụng hàm norm L2 của numpy.
     vector = vector1 - vector2
-    dist = np.linalg.norm(vector.tolist())
+    dist = np.linalg.norm(vector.tolist())      # Tính khoảng cách giữa 2 ảnh theo ct Euclid
     return dist
+
+
+def compare_2image(img1, img2):             # Hàm so sánh 2 ảnh.
+    dist = get_dist_2vector(img1, img2)     # Tính khoảng cách Euclid giữa 2 ảnh, ảnh càng giống thì dist càng nhỏ.
+    simi_percent = 100 * (1 - dist)         # Tính phần trăm độ giống nhau giữa 2 ảnh.
+    return simi_percent
 
 
 # Hiện tại ảnh em đang giải nén ra và để trong thư mục images. Bên anh để ở thư mục nào thì sửa lại  IMAGES_PATH
 IMAGES_PATH = './images/=Signatures/=GFD/'
 
-# Đọc các file trong thư mục
-filenames = os.listdir(IMAGES_PATH)
+# Tính độ giống nhau của 2 ảnh.
 
-# Chuyển toàn bộ các file thành vector cho vào mảng vector
-vectors = {}
-for name in filenames:
-    vector = get_vector(IMAGES_PATH + name)
-    vectors[name] = vector
-
-# So sánh ảnh 1 với các ảnh còn lại và vẽ đồ thị.
-percents = []
-for x in filenames:
-    percents.append(get_dist_2vector(vectors['s05n001.gfd'], vectors[x]))
-
-plt.plot([_ for _ in range(len(percents))], percents, 'r1')
-plt.xlabel('Images')
-plt.ylabel('Distance')
-plt.show()
+img1 = input("Image 1: ")               # Nhập vào tên ảnh 1
+img2 = input("Image 2: ")               # Nhập vào tên ảnh 2
+vec1 = get_vector(IMAGES_PATH + img1)   # Lấy vector dựa vào ảnh
+vec2 = get_vector(IMAGES_PATH + img2)   # Lấy vector dựa vào ảnh.
+simi_percent = compare_2image(vec1, vec2)   #Tính độ giống nhau giữa 2 vector
+print("The similarity of " + img1 + " and " + img2 + " is: " + str(round(simi_percent,2)) + "%")
 
